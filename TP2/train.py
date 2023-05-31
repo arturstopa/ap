ee6f99d0d7d0f8a12ea032397cb94a1151622e4c
Artur Stopa <arturstopa10@gmail.com>
2023-05-31 17:31:29 +0100

    param update

--- TP2/train.py
+++ TP2/train.py
@@ -3,9 +3,10 @@ from hewra import HeuristicAgent
 from snake_game import SnakeGame
 
 if __name__ == "__main__":
-    REPLAY_MEMORY_SIZE = 2**10
-    EPISODES = 100
-    EPISODE_TO_SAVE = 20
+    REPLAY_MEMORY_SIZE = 2**16
+    EPISODES = 30000
+    EPISODE_TO_SAVE_LOG = 1000
+    EPISODE_TO_SAVE_MODEL = 5000
     env = SnakeGame(14, 14, food_amount=1, grass_growth=0.001, max_grass=0.005)
     hewra = HeuristicAgent(env)
     agent = DqnAgent(env, replay_memory_size=REPLAY_MEMORY_SIZE)
@@ -18,6 +19,6 @@ if __name__ == "__main__":
         learning_rate=0.03,
         discount_factor=0.6,
         initial_examples=initial_examples,
-        episodes_to_save_log=EPISODE_TO_SAVE,
-        episodes_to_save_model=EPISODE_TO_SAVE,
+        episodes_to_save_log=EPISODE_TO_SAVE_LOG,
+        episodes_to_save_model=EPISODE_TO_SAVE_MODEL,
     )
